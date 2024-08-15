f926c7ccb4e12a00502eb8ad71bc8891e62c9eb6
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-08-15 18:11:49 +0300

    Completes Lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,41 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:number>')
+def count(number):
+    numbers = map(str, range(number))
+    return '\n'.join(numbers)  + '\n'
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+    except ValueError:
+        return "Please enter valid intergers"
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Invalid Operator"
+
+    return str(result)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
